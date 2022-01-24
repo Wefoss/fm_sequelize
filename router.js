68b15efa2b3c8bd4e8a76179913d5ae7be43dc2a
Timofii <timoxafos@gmail.com>
2022-01-24 12:15:15 +0200

    to tie tasks

--- router.js
+++ router.js
@@ -1,5 +1,6 @@
 const {Router} = require('express')
 const UserController = require('./controllers/user.controllers')
+const TaskContriller = require('./controllers/task.controller')
 
 const router = Router()
 
@@ -7,5 +8,7 @@ router.post('/user', UserController.createUser)
 router.get('/users', UserController.getAllUsers)
 router.patch('/user/:id', UserController.updateUser)
 router.patch('/user-in/:id', UserController.updateUserInstance)
+
+router.post('/user/:id/task', TaskContriller.createTask)
 
 module.exports = router
